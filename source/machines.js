e366dd2de6c09839121735680ed7543a70ddba32
Yasser4440 <85234823+Yasser4440@users.noreply.github.com>
2026-08-02 15:27:14 +0300

    Machine Textures
    Added some machines and multiblock textures
    Mapped single block machine textures
    Mapped the electric blast furnace texture

---
+++ source/machines.js
@@ -0,0 +1,114 @@
+export default machines; const machines = {
+    // Ore Processing
+    forge_hammer: {
+        front: 'forge_hammer',
+    },
+    macerator: {
+        front: 'macerator',
+        top: 'macerator_top',
+    },
+    ore_washer: {},
+    thermal_centrifuge: {},
+    electromagnetic_separator: {},
+    sifter: {
+        front: 'sifter',
+        front_top: 'sifter_top',
+    },
+    // Factory Machines
+    bender: {
+        front: 'bender',
+    },
+    wiremill: {
+        front: 'wiremill',
+        side: 'wiremill_side',
+        top: 'wiremill_top',
+    },
+    lathe: {
+        front: 'lathe',
+    },
+    polarizer: {},
+    cutter: {},
+    extruder: {},
+    extractor: {},
+    alloy_smelter: {
+        front: 'alloy_smelter',
+    },
+    arc_furnace: {
+        front: 'arc_furnace',
+        side: 'arc_furnace_side',
+        back: 'arc_furnace_side',
+        bottom: 'arc_furnace_bottom',
+    },
+    fluid_solidifier: {
+        front: 'fluid_solidifier',
+    },
+    forming_press: {},
+    laser_engraver: {},
+    assembler: {
+        front: 'assembler',
+        top: 'assembler_top',
+    },
+    circuit_assembler: {
+        front: 'circuit_assembler',
+        top: 'circuit_assembler_top',
+    },
+    scanner: {},
+    // Chemical Machines
+    chemical_reactor: {
+        front: 'chemical_reactor',
+    },
+    mixer: {
+        front: 'mixer',
+        side: 'mixer',
+        top: 'mixer_top',
+    },
+    electrolyzer: {
+        front: 'electrolyzer',
+        side: 'electrolyzer_side',
+    },
+    centrifuge: {
+        front: 'centrifuge',
+        side: 'centrifuge',
+        top: 'centrifuge_top',
+    },
+    brewery: {
+        front: 'brewery',
+        side: 'brewery',
+    },
+    autoclave: {},
+    distillery: {},
+    fermenter: {},
+    compressor: {},
+    fluid_heater: {},
+    // Utility Machines
+    electric_furnace: {
+        front: 'electric_furnace',
+    },
+    item_collector: {},
+    gas_collector: {
+        front: 'gas_collector',
+        side: 'gas_collector',
+        back: 'gas_collector',
+        top: 'gas_collector_top',
+        bottom: 'gas_collector_bottom',
+    },
+    fisher: {},
+    rock_crusher: {},
+    block_breaker: {},
+    world_accelerator: {
+        front: 'world_accelerator',
+        side: 'world_accelerator',
+        back: 'world_accelerator',
+    },
+    pump: {},
+    packer: {},
+    canner: {
+        front: 'canner',
+        side: 'canner_side',
+    },
+    
+    // air_scrubber: {},
+    // essence_burner: {},
+    // mystical_greenhouse: {},
+    // me_assembler: {},
+}
